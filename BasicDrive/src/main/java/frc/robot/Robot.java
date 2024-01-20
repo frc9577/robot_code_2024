8a07e74b2f422bc330e10354c8a88058897bfe4b
Recoil Robotics <recoilrobotics@gmail.com>
2024-01-20 15:39:59 -0600

    Removed use of depricated motor controller group class

--- BasicDrive/src/main/java/frc/robot/Robot.java
+++ BasicDrive/src/main/java/frc/robot/Robot.java
@@ -10,12 +10,7 @@ import edu.wpi.first.cscore.VideoSource.ConnectionStrategy;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
-import edu.wpi.first.wpilibj.motorcontrol.MotorController;
-import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
 import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
-
-import edu.wpi.first.cameraserver.CameraServer;
-import edu.wpi.first.cscore.UsbCamera;
 
 
 /**
@@ -27,14 +22,13 @@ public class Robot extends TimedRobot {
   private Joystick m_leftStick;
   private Joystick m_rightStick;
 
-  private final MotorController m_leftFrontMotor  = new WPI_VictorSPX(10);
-  private final MotorController m_leftBackMotor   = new WPI_VictorSPX(11);
-  private final MotorController m_rightFrontMotor = new WPI_VictorSPX(20);
-  private final MotorController m_rightBackMotor  = new WPI_VictorSPX(21);
+  private final WPI_VictorSPX m_leftFrontMotor  = new WPI_VictorSPX(10);
+  private final WPI_VictorSPX m_leftBackMotor   = new WPI_VictorSPX(11);
+  private final WPI_VictorSPX m_rightFrontMotor = new WPI_VictorSPX(20);
+  private final WPI_VictorSPX m_rightBackMotor  = new WPI_VictorSPX(21);
 
-  private final MotorControllerGroup m_leftMotorGroup  = new MotorControllerGroup(m_leftFrontMotor, m_leftBackMotor);
-  private final MotorControllerGroup m_rightMotorGroup = new MotorControllerGroup(m_rightFrontMotor, m_rightBackMotor);
 
+  
   /** Constants that define the settings of the driver camera */
   public static final int kDriverCameraResolutionX = 640;
   public static final int kDriverCameraResolutionY = 360;
@@ -47,9 +41,15 @@ public class Robot extends TimedRobot {
     // We need to invert one side of the drivetrain so that positive voltages
     // result in both sides moving forward. Depending on how your robot's
     // gearbox is constructed, you might have to invert the left side instead.
-    m_rightMotorGroup.setInverted(true);
+    
+    // NOTE: Invert needs to be done for both motors BEFORE follow
+    m_rightFrontMotor.setInverted(true);
+    m_rightBackMotor.setInverted(true);
 
-    m_myRobot = new DifferentialDrive(m_leftMotorGroup, m_rightMotorGroup);
+    m_leftBackMotor.follow(m_leftFrontMotor);
+    m_rightBackMotor.follow(m_rightFrontMotor);
+
+    m_myRobot = new DifferentialDrive(m_leftFrontMotor, m_rightFrontMotor);
     m_leftStick = new Joystick(0);
     m_rightStick = new Joystick(1);
 
